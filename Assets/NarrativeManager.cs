fcc20dc0861a2499a1ff2c59699058b2bf3b9f51
chrisjohns <chrisjohns210@gmail.com>
2018-06-09 16:42:46 -0700

    Added time system, fixed UI issues, added rooms and options based on current room

---
+++ Assets/NarrativeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrativeManager : MonoBehaviour {
+
+    public GameObject settingManager;
+    public Text mainText;
+
+    string[] observations = new string[10];
+
+	// Use this for initialization
+	void Start () {
+        observations[0] = "You look around your room. Your only possessions were already on your person. " +
+            "In the corner lay a bed of fur. Stone bricks covered every other surface. There was no door " +
+            "to the winding stairway that lead to the top.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GetObservation()
+    {
+        mainText.text = observations[settingManager.GetComponent<SettingManager>().GetRoom()];
+    }
+}
---
+++ Assets/ObserveOption.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserveOption : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Observe()
+    {
+        GameObject.Find("NarrativeManager").GetComponent<NarrativeManager>().GetObservation();
+    }
+}
